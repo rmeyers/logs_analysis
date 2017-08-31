e811865576c9f244c649552d6c5f4a0bd21fb461
vagrant <vagrant@vagrant.vm>
2017-08-31 01:54:16 +0000

    Update code based on Udacity submission feedback

--- README.md
+++ README.md
@@ -7,28 +7,44 @@ By Ryan Meyers
 ## About
 
 This is the first project in the section "The Backend" in the Udacity course for Fullstack Web Development.
+The script in run.py uses psycopg2 to query a mock PostgreSQL database for a fictional news website.
+The news database contains three tables, articles, authors, and log. Using these tables, the script in run.py
+answers the following three questions:
 
+1. What are the most popular three articles of all time?
+Which articles have been accessed the most? Present this
+information as a sorted list with the most popular article
+at the top.
+2. Who are the most popular article authors of all time?
+That is, when you sum up all of the articles each author
+has written, which authors get the most page views? Present
+this as a sorted list with the most popular author at the top.
+3. On which days did more than 1% of requests lead to errors?
+The log table includes a column status that indicates the HTTP
+status code that the news site sent to the user's browser.
 
 ## To Run
 
 ### You will need:
 
-* Python 2.7
-* Vagrant
-* VirtualBox
+* [Python 2.7](https://www.python.org/download/releases/2.7/)
+* [Vagrant](https://www.vagrantup.com/downloads.html)
+* [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
 
 
 ### Setup
 
 1. Install Vagrant And VirtualBox
-2. Clone this repository
+2. [Clone this repository](https://github.com/rmeyers/logs_analysis.git)
 
 
 ### To Run
 
-Launch Vagrant VM by running vagrant up, you can the log in with vagrant ssh
+Launch Vagrant VM by running `vagrant up`, you can the log in with `vagrant ssh`
 
-To load the data, use the command psql -d news -f newsdata.sql to connect a database and run the necessary SQL statements.
+To load the data, use the command `psql -d news -f newsdata.sql` to connect a database and run the necessary SQL statements.
+
+The database can be downloaded [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
 
 The database includes three tables:
 
@@ -36,4 +52,4 @@ The database includes three tables:
 * Articles table
 * Log table
 
-To execute the program, run python run.py from the command line.
+To execute the program, run `python run.py` from the command line.
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # This code is used running Python version 2.7
 
 import psycopg2
@@ -9,11 +10,13 @@ from datetime import datetime as dt
 # at the top.
 
 command1 = (
-    "SELECT articles.title, COUNT(*) AS views "
-    "FROM articles INNER JOIN log ON log.path "
-    "LIKE concat('%', articles.slug, '%') "
-    "WHERE log.status LIKE '%200%' GROUP BY "
-    "articles.title, log.path ORDER BY views DESC LIMIT 3")
+    """SELECT articles.title, COUNT(*) AS views
+    FROM articles INNER JOIN log
+    ON log.path = '/article/' || articles.slug
+    WHERE log.status LIKE '%200%'
+    GROUP BY articles.title, log.path
+    ORDER BY views DESC
+    LIMIT 3""")
 
 # 2. Who are the most popular article authors of all time?
 # That is, when you sum up all of the articles each author
@@ -21,60 +24,82 @@ command1 = (
 # this as a sorted list with the most popular author at the top.
 
 command2 = (
-    "SELECT authors.name, COUNT(*) AS views "
-    "FROM articles INNER JOIN authors ON "
-    "articles.author = authors.id INNER JOIN log "
-    "ON log.path LIKE concat('%', articles.slug, '%') WHERE "
-    "log.status LIKE '%200%' GROUP "
-    "BY authors.name ORDER BY views DESC")
+    """SELECT authors.name, COUNT(*) AS views
+    FROM articles INNER JOIN authors
+    ON articles.author = authors.id INNER JOIN log
+    ON log.path = '/article/' || articles.slug
+    WHERE log.status LIKE '%200%' GROUP
+    BY authors.name ORDER BY views DESC""")
 
 # 3. On which days did more than 1% of requests lead to errors?
 # The log table includes a column status that indicates the HTTP
 # status code that the news site sent to the user's browser.
 
 command3 = (
-    "SELECT day, perc FROM ("
-    "SELECT day, round((sum(requests)/(SELECT COUNT(*) FROM log WHERE "
-    "substring(cast(log.time AS text), 0, 11) = day) * 100), 2) AS "
-    "perc FROM (SELECT substring(cast(log.time as text), 0, 11) AS day, "
-    "COUNT(*) AS requests FROM log WHERE status LIKE '%404%' GROUP BY day)"
-    "AS log_percentage GROUP BY day ORDER BY perc DESC) AS final_query "
-    "WHERE perc >= 1")
+    """SELECT day, perc FROM (
+    SELECT day, round((sum(requests)/(SELECT COUNT(*)
+    FROM log
+    WHERE substring(cast(log.time AS text), 0, 11) = day) * 100), 2) AS perc
+    FROM (SELECT substring(cast(log.time as text), 0, 11) AS day, COUNT(*)
+    AS requests FROM log WHERE status
+    LIKE '%404%' GROUP BY day)
+    AS log_percentage GROUP BY day
+    ORDER BY perc DESC) AS final_query
+    WHERE perc >= 1""")
+
+# command4 = (
+#     """SELECT date, errors/requests
+#     WHERE errors/requests >= 0.01
+#     (FROM SELECT time::date AS date, count(*) AS errors
+#     FROM log
+#     WHERE status = '404 NOT FOUND'
+#     GROUP BY date) AND
+#     (SELECT time::date AS date, count(*) AS requests
+#     FROM log
+#     GROUP BY date;
+#     """)
 
 
 def run_query(command):
-    conn = psycopg2.connect("dbname=news")
+    ''' This runs the query on the 'news' database, fetches all
+    results, and closes the DB connection. '''
+    try:
+        conn = psycopg2.connect("dbname=news")
+    except psycopg2.Error as e:
+        print "Error: ", e
+        return "Unable to connect to the database."
     cur = conn.cursor()
-
     cur.execute(command)
-
-    return cur.fetchall()
-
+    results = cur.fetchall()
     conn.close()
 
+    return results
 
 if __name__ == '__main__':
     request1 = run_query(command1)
-    num1 = str(request1[0][1])
-    num2 = str(request1[1][1])
-    num3 = str(request1[2][1])
-    print "The three most popular articles of all time are: \n1. " +\
-        request1[0][0] + " - " + num1 + "views\n2. " + request1[1][0] + \
-        " - " + num2 + "views, and \n3. " + request1[2][0] + " - " + \
-        num3 + "views"
+    print("The three most popular articles of all time are: ")
+    print("-" * 15)
+    for result in request1:
+        print("\"{}\" - {} views".format(result[0], result[1]))
 
     request2 = run_query(command2)
-    num1 = str(request2[0][1])
-    num2 = str(request2[1][1])
-    num3 = str(request2[2][1])
-    print "\nThe three most popular article authors of all time are: \n1. " +\
-        request2[0][0] + " - " + num1 + "views\n2. " + request2[1][0] +\
-        " - " + num2 + "views, and \n3. " + request2[2][0] + " - " + num3 +\
-        "views"
+    print("\nThe three most popular article authors of all time are: ")
+    print("-" * 15)
+    for result in request2:
+        print("{} - {} views".format(result[0], result[1]))
 
     request3 = run_query(command3)
-    date = request3[0][0]
-    date_object = dt.strptime(date, '%Y-%m-%d')
-    percentage = str(request3[0][1])
-    print "\nDays with more than 1% of requests leading to errors: \n" + \
-        date_object.strftime("%d %B %Y") + " - " + percentage + "%"
+    print("\nDays with more than 1% of requests leading to errors: ")
+    print("-" * 15)
+    for result in request3:
+        date = result[0]
+        date_object = dt.strptime(date, '%Y-%m-%d')
+        percentage = str(result[1])
+        print("{} - {}%").format(date_object.strftime("%d %B %Y"), percentage)
+
+    # request4 = run_query(command4)
+    # print request4
+
+
+
+
